6eaa18e93bc7117838e02c8d0b672e7f42b4e3dd
Alete Dini <maildealete@gmail.com>
2017-07-02 13:20:36 -0300

    la clase camaras398 ahora es pública y provee una lista de modelos según marca

--- 398_UI/camaras988.cs
+++ 398_UI/camaras988.cs
@@ -7,23 +7,33 @@ using System.ComponentModel;
 
 namespace _398_UI
 {
-    class camaras398
+    public class camaras398
     {
         public string marca { get; set; }
-        List<string> modelos { get; set; }
+        public List<string> modelos { get; set; }
 
-    public static BindingList<camaras398> lista()
+        public static BindingList<camaras398> lista()
         {
-            return IO.readJsonList<camaras398>(@"..\..\camaras3.txt");
+            return IO.readJsonList<camaras398>(@"..\..\camaras398.txt");
         }
         public static List<string> listaDeMarcas()
         {
             List<string> marcas = new List<string>();
-            foreach(camaras398 cam in lista())
+            foreach (camaras398 cam in lista())
             {
                 marcas.Add(cam.marca);
             }
             return marcas;
+        }
+
+        internal static List<string> listaDeModelos(int selectedIndex)
+        {
+            List<string> modelosDeEstaMarca = new List<string>();
+            foreach (string modelo in lista().ElementAt(selectedIndex).modelos)
+            {
+                modelosDeEstaMarca.Add(modelo);
+            }
+            return modelosDeEstaMarca;
         }
     }
 }
